b1fe0825425d798039ab5236380900b9ec0939d4
MykhailoKutsyi <91030243+MykhailoKutsyi@users.noreply.github.com>
2022-08-17 19:53:01 +0300

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-it('should render a button', () => {
+it('should render a heading', () => {
   render(<App />);
-  const linkElement = screen.getByRole('button');
+  const linkElement = screen.getByRole('heading');
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('should render a list', () => {
+  render(<App />);
+  const linkElement = screen.getByRole('list');
   expect(linkElement).toBeInTheDocument();
 });
 
@@ -13,11 +18,3 @@ it('should render a button', () => {
   const linkElement = screen.getByRole('button');
   expect(linkElement).toBeInTheDocument();
 });
-
-// it('should render correctly with title and description', () => {
-//   const wrapper = shallow(
-//     <App title="Some title" description="Some description" />
-//   );
-
-//   expect(toJson(wrapper)).toMatchSnapshot();
-// });
